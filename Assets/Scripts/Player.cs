c59d43b40533e1a8bb590dc552873bdbde3697a7
hyuncho <84334547+aquamagic9@users.noreply.github.com>
2025-01-25 20:41:02 +0900

    Feat: Jump, Fall 동작 및 애니메이션 적용

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ public class Player : MonoBehaviour
 {
     [Header("Move Info")]
     public float moveSpeed = 12f;
+    public float jumpForce = 6f;
 
     #region Components
     public Animator anim {  get; private set; }
@@ -14,6 +15,8 @@ public class Player : MonoBehaviour
 
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState { get; private set; }
+    public PlayerJumpState jumpState { get; private set; }
+    public PlayerAirState airState { get; private set; }
     #endregion
 
     private void Awake()
@@ -22,6 +25,8 @@ public class Player : MonoBehaviour
 
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
         moveState = new PlayerMoveState(this, stateMachine, "Move");
+        jumpState = new PlayerJumpState(this, stateMachine, "Jump");
+        airState = new PlayerAirState(this, stateMachine, "Jump");
     }
 
     private void Start()
