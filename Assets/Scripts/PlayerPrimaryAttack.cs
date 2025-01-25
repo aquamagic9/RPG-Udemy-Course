ebc6be389c30689016ec3a88e36b6ff78bb09b90
hyuncho <84334547+aquamagic9@users.noreply.github.com>
2025-01-26 03:50:06 +0900

    Feat: Combo Attack 기능 추가

--- Assets/Scripts/PlayerPrimaryAttack.cs
+++ Assets/Scripts/PlayerPrimaryAttack.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class PlayerPrimaryAttack : PlayerState
 {
+    private int comboCounter;
+
+    private float lastTimeAttacked;
+    private float comeboWindow = 2;
     public PlayerPrimaryAttack(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
     }
@@ -9,11 +13,19 @@ public class PlayerPrimaryAttack : PlayerState
     public override void Enter()
     {
         base.Enter();
+
+        if (comboCounter > 2 || Time.time >= lastTimeAttacked + comeboWindow)
+            comboCounter = 0;
+        
+        player.anim.SetInteger("ComboCounter", comboCounter);
     }
 
     public override void Exit()
     {
         base.Exit();
+
+        comboCounter++;
+        lastTimeAttacked = Time.time;
     }
 
     public override void Update()
