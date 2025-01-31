87eebb95180e50564f63c6c18bfe048fbc1d3f8f
aquamagic9 <84334547+aquamagic9@users.noreply.github.com>
2025-01-31 17:56:48 +0900

    Feat: 공격 충돌 판정 적용

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -8,6 +8,8 @@ public class Entity : MonoBehaviour
     #endregion
 
     [Header("Collision Info")]
+    public Transform attackCheck;
+    public float attackCheckRadius;
     [SerializeField] protected Transform groundCheck;
     [SerializeField] protected float groundCheckDistance;
     [SerializeField] protected Transform wallCheck;
@@ -32,6 +34,11 @@ public class Entity : MonoBehaviour
     {
 
     }
+    
+    public virtual void Damage()
+    {
+        Debug.Log(gameObject.name + " took damage");
+    }
 
     #region Velocity
     public void SetZeroVelocity() => rb.linearVelocity = new Vector2(0,0);
@@ -51,6 +58,7 @@ public class Entity : MonoBehaviour
     {
         Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
         Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+        Gizmos.DrawWireSphere(attackCheck.position, attackCheckRadius);
     }
     #endregion
 
